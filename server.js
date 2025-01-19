cfa7620b7fb99d2813e9cc03b640879e1a91cef4
PEARLYNN TAN SZE LIN <22000484@myrp.edu.sg>
2025-01-20 00:30:48 +0800

    Hash and compare passwords asynchronously

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
-const bcrypt      = require("bcrypt");
+const bcrypt      = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
