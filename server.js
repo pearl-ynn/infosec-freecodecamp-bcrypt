98d662bc2d3c2030d4b6845c7704f7be1f2f4a7e
PEARLYNN TAN SZE LIN <22000484@myrp.edu.sg>
2025-01-20 20:24:14 +0800

    work plz

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+
 bcrypt.hash(someOtherPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
   bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
