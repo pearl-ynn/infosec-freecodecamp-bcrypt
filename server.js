5a606d346374ce6ea65075851b14f94e2957041d
PEARLYNN TAN SZE LIN <22000484@myrp.edu.sg>
2025-01-20 20:27:37 +0800

    PLZ

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(someOtherPlaintextPassword, saltRounds, (err, hash) => {
   console.log(hash);
